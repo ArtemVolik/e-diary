a0af61ee468b55fd757300eb9a2c780fbede5a26
artyom <art.volyk@gmail.com>
2020-11-21 23:13:58 +0200

    replace randint with order_by('?')

--- scripts.py
+++ scripts.py
@@ -1,5 +1,4 @@
 from datacenter.models import Schoolkid, Chastisement, Commendation, Lesson, Mark
-from random import randint
 
 
 def get_schoolkid(schoolkid: str):
@@ -28,9 +27,8 @@ def remove_chastisements(schoolkid: str):
 
 def create_commendation(schoolkid: str, subject: str):
     child = get_schoolkid(schoolkid)
-    lessons = Lesson.objects.filter(year_of_study=6, group_letter='А', subject__title=subject)
-    lesson_number = randint(0, lessons.count())
-    chosen_lesson = lessons[lesson_number]
+    lessons = Lesson.objects.filter(year_of_study=6, group_letter='А', subject__title=subject).order_by('?')
+    chosen_lesson = lessons[0]
     commendation_texts = ['Молодец', 'Гораздо лучше, чем я ожидал!', 'Сказано здорово – просто и ясно!',
                           'Ты сегодня прыгнул выше головы!', 'Я тобой горжусь']
     commendation_number = randint(0, len(commendation_texts))
